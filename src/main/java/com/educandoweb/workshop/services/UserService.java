8a4c576e908fa1ef897f630cce9a6395f65f53e1
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-07-17 13:01:02 -0300

    User Update

--- src/main/java/com/educandoweb/workshop/services/UserService.java
+++ src/main/java/com/educandoweb/workshop/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.workshop.entities.User;
@@ -31,5 +32,17 @@ public class UserService {
 	
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+	
+	public User update(Long id, User user) {
+		User entity = userRepository.getReferenceById(id);	
+		updateData(entity, user);
+		return userRepository.save(entity);
+	}
+	
+	private void updateData(User updatedUser, User referenceUser) {
+		updatedUser.setEmail(referenceUser.getEmail());
+		updatedUser.setName(referenceUser.getName());
+		updatedUser.setPhone(referenceUser.getPhone());
 	}
 }
